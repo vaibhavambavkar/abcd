0dc6f6f7581a1900ed77963b676a8bff436924f0
vaibhavambavkar <vaibhav.ambavkar@vpmmpcoe.org>
2015-12-22 22:51:50 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$link = mysql_connect('mysql77757-env-3332420.jelasticlw.com.br', 'root', '26dZNC81L7');
+//if connection is not successful you will see text error
+if (!$link) {
+       die('Could not connect: ' . mysql_error());
+}
+//if connection is successfuly you will see message bellow
+echo 'Connected successfully';
+ 
+mysql_close($link);
+?>
