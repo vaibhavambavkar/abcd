f838795a410045714c9e2a46955d1becb2f53738
vaibhavambavkar <vaibhav.ambavkar@vpmmpcoe.org>
2015-12-30 20:25:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysql_connect('mysql77757-env-3332420.jelasticlw.com.br', 'root', '26dZNC81');
+$link = mysql_connect('mysql77757-env-3332420.jelasticlw.com.br', 'root', '26dZNC81L7');
 //if connection is not successful you will see text error
 if (!$link) {
        die('Could not connect: ' . mysql_error());
